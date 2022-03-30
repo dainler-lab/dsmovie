0154b78e04494d72cd2693f127519c7bb6a23a47
Dainler <dainler.ms@gmail.com>
2022-03-30 17:36:56 -0400

    Show score

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)/* id,desc */
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)/* id,desc */ /* &sort=title */
       .then(response => {
         const data = response.data as MoviePage;
         /* console.log(data);
